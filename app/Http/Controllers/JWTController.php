9aff0346b52a3dffdea4d89726f20609486beed4
Miguel-Lopez004 <60121970+MauricioAlba@users.noreply.github.com>
2023-10-13 21:12:33 -0400

    users complete

--- app/Http/Controllers/JWTController.php
+++ app/Http/Controllers/JWTController.php
@@ -30,11 +30,25 @@ class JWTController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
+
+     public function listUsers()
+    {
+        //muestra todos los usuaros
+        $user = DB::table('users')->get();
+        return $user;
+    }
+
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|min:2|max:100',
-            'email' => 'required|string|min:2|max:100',
+            'ciNumber' => 'required|string|min:2|max:100',
+            'firstName' => 'required|string|min:2|max:100',
+            'lastName' => 'required|string|min:2|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'phoneNumber' => 'required|string|min:2|max:100',
+            'role' => 'required|string|min:2|max:100',
+            'address' => 'required|string|min:2|max:100',
+            'profilePicture' => 'required|string|min:2|max:100',
             'password' => 'required|string|confirmed|min:6',
         ]);
 
@@ -43,21 +57,28 @@ class JWTController extends Controller
         }
 
         $user = User::create([
-            'name' => $request->name,
+            'ciNumber' => $request->ciNumber,
+            'firstName' => $request->firstName,
+            'lastName' => $request->lastName,
             'email' => $request->email,
+            'phoneNumber' => $request->phoneNumber,
+            'role' => $request->role,
+            'address' => $request->address,
+            'profilePicture' => $request->profilePicture,
+            'birthDate' => $request->birthDate,
             'password' => Hash::make($request->password)
-            
         ]);
 
         //envio email
 
-        //Mail::to($user->email)->send(new AccountCreated($user));
+        Mail::to($user->email)->send(new AccountCreated($user));
 
         return response()->json([
             'message' => 'Usuario registrado con exito',
             'user' => $user
         ], 201);
     }
+
 
     /**
      * login user
@@ -127,6 +148,57 @@ class JWTController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 1440,
+
+
+            "user" => [
+                "ciNumber" => auth('api')->user()->ciNumber,
+                "firstName" => auth('api')->user()->firstName,
+                "lastName" => auth('api')->user()->lastName,
+                "email" => auth('api')->user()->email,
+                "phoneNumber" => auth('api')->user()->phoneNumber,
+                "role" => auth('api')->user()->role,
+                //nos vota el la informacion del usuario y hace que se almacene en el local storage evita consultas continuas
+            ],
+            "idUser" => auth('api')->user()->idUser,
+            "ciNumber" => auth('api')->user()->ciNumber,
+            "firstName" => auth('api')->user()->firstName,
+            "lastName" => auth('api')->user()->lastName,
+            "role" => auth('api')->user()->role,
+            "email" => auth('api')->user()->email,
         ]);
     }
+
+    public function updatePassword(Request $request)
+    {
+        // Validación
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|min:8|confirmed',
+
+        ]);
+
+        // Obtener el usuario autenticado
+        $user = JWTAuth::parseToken()->authenticate();
+
+        // Verificar la contraseña anterior
+        if (!Hash::check($request->old_password, $user->password)) {
+            return response()->json(['error' => 'La contraseña anterior es incorrecta'], 401);
+        }
+
+        // Actualizar la nueva contraseña
+        $user->password = Hash::make($request->new_password);
+        $user->save();
+
+        return response()->json(['message' => 'Contraseña actualizada correctamente']);
+    }
+
+    public function userById(Request $request)  // obtener users por id
+    {
+        $user = DB::table('users')->where('idUser', '=', $request->idUser)
+            ->select('users.idUser', 'users.ciNumber', 'users.firstName', 'users.lastName', 'users.email', 'users.phoneNumber', 'users.role', 'users.address', 'users.profilePicture', 'users.birthDate')
+            ->get();
+
+        return $user;
+    }
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id('idUser');
+            
+            $table->string('ciNumber');
+            $table->string('firstName');
+            $table->string('lastName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phoneNumber');
+            $table->string('address');
+            $table->string('profilePicture');
+            $table->date('birthDate');
+            $table->string('role');
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps(); 
+
         });
     }
 
