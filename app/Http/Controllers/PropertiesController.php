2206223c3538c8da2d04c2d2176054ad40a0da81
adan llanos <adanllanos96@gmail.com>
2023-11-19 09:40:29 -0400

    agregando nuevos campos a la propiedad

--- app/Http/Controllers/PropertiesController.php
+++ app/Http/Controllers/PropertiesController.php
@@ -15,101 +15,120 @@ class PropertiesController extends Controller
 {
     public function createdProperties(Request $request)
     {
-        $validator = Validator::make($request->all(), [  //valida los maximos y
-            'propertyName' => 'required|string|min:1|max:50',
-            'propertyOperation' => 'required|string|min:1|max:100',
-            'propertyType' => 'required|string|min:1|max:100',
-            'propertyAddress' => 'required|string|min:1|max:100',
-            'propertyDescription' => 'required|string|min:1|max:200',
-            'propertyServices' => 'required|string|min:1|max:100',
-            'propertyStatus' => 'required|string|min:1|max:100',
-            'propertyAmount' => 'required|integer|min:0',
-            'propertyAbility' => 'required|integer|min:0',
-            'propertyCity' => 'required',
-            'host_id' => 'required|integer|min:0'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $property = new properties([
-            'propertyName' => $request->propertyName,
-            'propertyOperation' => $request->propertyOperation,
-            'propertyType' => $request->propertyType,
-            'propertyAddress' => $request->propertyAddress,
-            'propertyDescription' => $request->propertyDescription,
-            'propertyServices' => $request->propertyServices,
-            'propertyStatus' => $request->propertyStatus,
-            'propertyAmount' => $request->propertyAmount,
-            'propertyCity' => $request->propertyCity,
-            'propertyAbility' => $request->propertyAbility,
-
-        ]);
-        $property->host_id = $request->host_id;
-        //$property = Properties::create($request->all());
-        $property->save();
-
-
-        $propertyId = $property->idProperty;
-
-        $holidays = $request->input('holidays');
-
-        if (!is_array($holidays)) {
-            return response()->json([
-                'message' => 'El campo holidays debe ser un array válido.',
-            ], 400);
-        }
-
-        foreach ($holidays as $holidayData) {
-            $holiday = new Holidays([
-                'startDate' => $holidayData['startDate'],
-                'endDate' => $holidayData['endDate'],
-                'status' => $holidayData['status'],
-                'amount' => $holidayData['amount'],
+        try {
+            $validator = Validator::make($request->all(), [  //valida los maximos y
+                'propertyName' => 'required|string|min:1|max:50',
+                'propertyOperation' => 'required|string|min:1|max:100',
+                'propertyType' => 'required|string|min:1|max:100',
+                'propertyAddress' => 'required|string|min:1|max:100',
+                'propertyDescription' => 'required|string|min:1|max:200',
+                'propertyServices' => 'required|string|min:1|max:100',
+                'propertyStatus' => 'required|string|min:1|max:100',
+                'propertyAmount' => 'required|integer|min:0',
+                'propertyAbility' => 'required|integer|min:0',
+                'propertyCity' => 'required',
+                'propertyCroquis' => 'required|string',
+                'propertyRooms' => 'required|integer|min:1',
+                'propertyBathrooms' => 'required|integer|min:1',
+                'propertyBeds' => 'required|integer|min:1',
+                'propertyRules' => 'required|string',
+                'propertySecurity' => 'required|string',
+                'host_id' => 'required|integer|min:0'
             ]);
-            if (!empty($propertyId)) {
-                $holiday->property_id = $propertyId;
-            } else {
-                return response()->json([
-                    'message' => 'el id de la propieda no existe',
-                    'propertyId' => $propertyId,
-                ], 201);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
             }
-            $holiday->save();
-        }
-        $images =  $request->input('images');
 
-        if (!is_array($images)) {
-            return response()->json([
-                'message' => 'El campo images debe ser un array válido.',
-            ], 400);
-        }
+            $property = new properties([
+                'propertyName' => $request->propertyName,
+                'propertyOperation' => $request->propertyOperation,
+                'propertyType' => $request->propertyType,
+                'propertyAddress' => $request->propertyAddress,
+                'propertyDescription' => $request->propertyDescription,
+                'propertyServices' => $request->propertyServices,
+                'propertyStatus' => $request->propertyStatus,
+                'propertyAmount' => $request->propertyAmount,
+                'propertyCity' => $request->propertyCity,
+                'propertyAbility' => $request->propertyAbility,
+                'propertyCroquis' => $request->propertyCroquis,
+                'propertyRooms' => $request->propertyRooms,
+                'propertyBathrooms' => $request->propertyBathrooms,
+                'propertyBeds' => $request->propertyBeds,
+                'propertyRules' => $request->propertyRules,
+                'propertySecurity' => $request->propertySecurity,
 
-        foreach ($images as $image) {
-            $image_property = new Images([
-                'imageLink' => $image['imageLink'],
-                'imageDescription' => $image['imageDescription'],
             ]);
-            if (!empty($propertyId)) {
-                $image_property->property_id = $propertyId;
+            $property->host_id = $request->host_id;
+            //$property = Properties::create($request->all());
+            $property->save();
+
+
+            $propertyId = $property->idProperty;
+
+            $holidays = $request->input('holidays');
+
+            if (!is_array($holidays)) {
+                return response()->json([
+                    'message' => 'El campo holidays debe ser un array válido.',
+                ], 400);
+            }
+
+            foreach ($holidays as $holidayData) {
+                $holiday = new Holidays([
+                    'startDate' => $holidayData['startDate'],
+                    'endDate' => $holidayData['endDate'],
+                    'status' => $holidayData['status'],
+                    'amount' => $holidayData['amount'],
+                ]);
+                if (!empty($propertyId)) {
+                    $holiday->property_id = $propertyId;
+                } else {
+                    return response()->json([
+                        'message' => 'el id de la propieda no existe',
+                        'propertyId' => $propertyId,
+                    ], 201);
+                }
+                $holiday->save();
+            }
+            $images =  $request->input('images');
+
+            if (!is_array($images)) {
+                return response()->json([
+                    'message' => 'El campo images debe ser un array válido.',
+                ], 400);
+            }
+
+            foreach ($images as $image) {
+                $image_property = new Images([
+                    'imageLink' => $image['imageLink'],
+                    'imageDescription' => $image['imageDescription'],
+                ]);
+                if (!empty($propertyId)) {
+                    $image_property->property_id = $propertyId;
+                    $image_property->save();
+                } else {
+                    return response()->json([
+                        'message' => 'El ID de la propiedad no existe',
+                        'propertyId' => $propertyId,
+                    ], 400); // Usar un código de respuesta 400 para errores
+                }
                 $image_property->save();
-            } else {
-                return response()->json([
-                    'message' => 'El ID de la propiedad no existe',
-                    'propertyId' => $propertyId,
-                ], 400); // Usar un código de respuesta 400 para errores
             }
-            $image_property->save();
+
+
+            return response()->json([
+                'message' => 'successful property registration.',
+                'properties' => $property,
+                'holidays' => $holidays,
+                'Images_Property' => $images,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al registrar la propiedad.',
+                'error' => $e->getMessage(),
+            ], 500);
         }
-
-
-        return response()->json([
-            'message' => 'successful property registration.',
-            'properties' => $property,
-            'holidays' => $holidays,
-            'Images_Property' => $images,
-        ], 201);
     }
 
     public function getAllProperties()
