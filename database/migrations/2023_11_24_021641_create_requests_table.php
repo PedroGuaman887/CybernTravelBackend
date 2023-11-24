1cf8beb7c512659a8b988327bf5e697a97a73b1f
PedroGuaman887 <142186975+PedroGuaman887@users.noreply.github.com>
2023-11-23 23:38:47 -0400

    subiendo migracion  y modelo de requests

---
+++ database/migrations/2023_11_24_021641_create_requests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id('idRequests');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->date('dateRequest');
+            $table->string('status');
+            $table->unsignedBigInteger('idProperty');
+            $table->unsignedBigInteger('idUser');
+            $table->foreign('idProperty')->references('idProperty')->on('properties');
+            $table->foreign('idUser')->references('idUser')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
