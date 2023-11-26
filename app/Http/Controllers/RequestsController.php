1fd36a69d5d01cc5c12ae80d95f996e619ba20da
mauricio-alba <60121970+MauricioAlba@users.noreply.github.com>
2023-11-25 23:40:09 -0400

    reservations

--- app/Http/Controllers/RequestsController.php
+++ app/Http/Controllers/RequestsController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Requests;
+use App\Models\Properties;
+use App\Models\User;
+use App\Models\Reservations;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
@@ -12,9 +16,8 @@ class RequestsController extends Controller
     public function createdRequests(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            //'startDate' => 'date_format:Y-m-d',
-            //'endDate' => 'date_format:Y-m-d',
-            //'dateRequest' => 'date_format:Y-m-d',
+            'startDate' => 'date_format:Y-m-d',
+            'endDate' => 'date_format:Y-m-d',
             'status' => 'required|string',
         ]);
 
@@ -22,17 +25,20 @@ class RequestsController extends Controller
             return response()->json($validator->errors(), 400);
         }
 
-        $newRequests = new requests([
+        // Sumar un día a la fecha
+        $dateRequest = Carbon::parse($request->dateRequest)->addDay();
 
+        $newRequests = new Requests([
             'startDate' => $request->startDate,
             'endDate' => $request->endDate,
-            'dateRequest' => $request->dateRequest,
+            'dateRequest' => $dateRequest,
             'status' => $request->status,
-            'idProperty' => $request->idProperty,
-            'idUser' => $request->idUser,
-
         ]);
 
+        $newRequests->idProperty = $request->idProperty;
+        $newRequests->idUser = $request->idUser;
+
+        $newRequests->save();
 
         return response()->json([
             'message' => 'User successfully request',
@@ -40,17 +46,41 @@ class RequestsController extends Controller
         ], 201);
     }
 
-    public function updateRequests(Request $request, $id)
+        public function updateRequests(Request $request, $id)
     {
-        $requests = requests::find($id);
+        $requests = Requests::find($id);
 
         $requests->startDate = $request->startDate;
         $requests->endDate = $request->endDate;
         $requests->dateRequest = $request->dateRequest;
         $requests->status = $request->status;
-        
+
         $requests->save();
-        return $requests;
+
+        // Verificar si el estado es 'Pagado'
+        if ($request->status === 'Pagado') {
+            // Crear una nueva reserva
+            $newReservation = new Reservations([
+                'startDate' => $request->startDate,
+                'endDate' => $request->endDate,
+                'datePayment' => now(), // Puedes ajustar esto según tu lógica
+                'status' => 'nueva', // Iniciar el estado con 'nueva'
+                'idRequests' => $id, // Utilizar el ID de la solicitud actual
+            ]);
+
+            $newReservation->save();
+
+            return response()->json([
+                'message' => 'User successfully updated and reservation created',
+                'requests' => $requests,
+                'reservation' => $newReservation,
+            ], 201);
+        }
+
+        return response()->json([
+            'message' => 'User successfully updated, but no reservation created',
+            'requests' => $requests,
+        ], 200);
     }
 
     public function deleteRequests(Request $request)
@@ -115,7 +145,7 @@ class RequestsController extends Controller
         foreach ($requests as $request) {
 
             if ($request->idUser !== null) {
-                $user = users::find($request->idUser);
+                $user = User::find($request->idUser);
                 $request->user = [
                     'idUser' => $user->idUser,
                     'fullName' => $user->fullName,
@@ -125,26 +155,26 @@ class RequestsController extends Controller
                 ];
             }
 
-            if ($request->idSitio !== null) {
-                $propertie = properties::find($request->idProperty);
-                $request->propertie = [
-                    'idProperty' => $propertie->idProperty,
-                    'propertyName' => $propertie->propertyName,
-                    'propertyOperation' => $propertie->propertyOperation,
-                    'propertyType' => $propertie->propertyType,
-                    'propertyAddress' => $propertie->propertyAddress,
-                    'propertyDescription' => $propertie->propertyDescription,
-                    'propertyServices' => $propertie->propertyServices,
-                    'propertyStatus' => $propertie->propertyStatus,
-                    'propertyAmount' => $propertie->propertyAmount,
-                    'propertyAbility' => $propertie->propertyAbility,
-                    'propertyCity' => $propertie->propertyCity,
-                    'propertyCroquis' => $propertie->propertyCroquis,
-                    'propertyRooms' => $propertie->propertyRooms,
-                    'propertyBathrooms' => $propertie->propertyBathrooms,
-                    'propertyBeds' => $propertie->propertyBeds,
-                    'propertyRules' => $propertie->propertyRules,
-                    'propertySecurity' => $propertie->propertySecurity
+            if ($request->idProperty !== null) {
+                $properties = Properties::find($request->idProperty);
+                $request->properties = [
+                    'idProperty' => $properties->idProperty,
+                    'propertyName' => $properties->propertyName,
+                    'propertyOperation' => $properties->propertyOperation,
+                    'propertyType' => $properties->propertyType,
+                    'propertyAddress' => $properties->propertyAddress,
+                    'propertyDescription' => $properties->propertyDescription,
+                    'propertyServices' => $properties->propertyServices,
+                    'propertyStatus' => $properties->propertyStatus,
+                    'propertyAmount' => $properties->propertyAmount,
+                    'propertyAbility' => $properties->propertyAbility,
+                    'propertyCity' => $properties->propertyCity,
+                    'propertyCroquis' => $properties->propertyCroquis,
+                    'propertyRooms' => $properties->propertyRooms,
+                    'propertyBathrooms' => $properties->propertyBathrooms,
+                    'propertyBeds' => $properties->propertyBeds,
+                    'propertyRules' => $properties->propertyRules,
+                    'propertySecurity' => $properties->propertySecurity
                 ];
             }
         }
